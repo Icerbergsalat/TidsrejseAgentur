708cdfb39d58932db47f8ba3d4a4b621a805b661
Icerbergsalat <multi1410@gmail.com>
2025-02-02 17:43:56 +0100

    gh

---
+++ src/main/java/com/example/tidsrejseagentur/KundeController.java
@@ -0,0 +1,26 @@
+package com.example.tidsrejseagentur;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class KundeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    protected void onReturnButtonClick(javafx.event.ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
